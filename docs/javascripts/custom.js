65ebbc4037e4838da5a122a6e2a15d6406f1672f
Ahmed Thahir <ahmedthahir2002@gmail.com>
2023-03-26 11:22:31 +0400

    Update custom.js

--- docs/javascripts/custom.js
+++ docs/javascripts/custom.js
@@ -1,17 +1,17 @@
-function prefetch(link, type)
-{
-  const prefetch = document.createElement("link");
-
-  prefetch.setAttribute("rel", "prefetch");
-  prefetch.setAttribute("href", link);
-  prefetch.setAttribute("as", type);
-  if(type == "font" && link.includes("http")) // online font
-    prefetch.setAttribute("crossorgin", true);
-
-  document.head.appendChild(prefetch);
-}
-
 window.addEventListener("load", function(){
+  function prefetch(link, type)
+  {
+    const prefetch = document.createElement("link");
+
+    prefetch.setAttribute("rel", "prefetch");
+    prefetch.setAttribute("href", link);
+    prefetch.setAttribute("as", type);
+    if(type == "font" && link.includes("http")) // online font
+      prefetch.setAttribute("crossorgin", true);
+
+    document.head.appendChild(prefetch);
+  }
+
   let next = document.querySelector(".md-footer__link--next").href;
   prefetch(next.toLowerCase(), "document");
 });
