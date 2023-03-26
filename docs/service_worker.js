53f811c72b2bf21fd873965a5b7b65ffc0c0bdff
Ahmed Thahir <ahmedthahir2002@gmail.com>
2023-03-26 17:50:06 +0400

    Update service_worker.js

--- docs/service_worker.js
+++ docs/service_worker.js
@@ -1,69 +1,70 @@
-// the cache version gets updated every time there is a new deployment
-const CACHE_VERSION = 10;
-const CURRENT_CACHE = `main-${CACHE_VERSION}`;
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-// these are the routes we are going to cache for offline support
-const cacheFiles = ['/'];
+const CACHE = "pwabuilder-offline-page";
 
-// on activation we clean up the previously registered service workers
-self.addEventListener('activate', evt =>
-  evt.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CURRENT_CACHE) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "https://uni-notes.netlify.app/";
+
+// Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Cached offline page during install");
+
+      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
+        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
+      }
+
+      return cache.add(offlineFallbackPage);
     })
-  )
-);
-
-// on install we download the routes we want to cache for offline
-self.addEventListener('install', evt =>
-  evt.waitUntil(
-    caches.open(CURRENT_CACHE).then(cache => {
-      return cache.addAll(cacheFiles);
-    })
-  )
-);
-
-// fetch the resource from the network
-const fromNetwork = (request, timeout) =>
-  new Promise((fulfill, reject) => {
-    const timeoutId = setTimeout(reject, timeout);
-    fetch(request).then(response => {
-      clearTimeout(timeoutId);
-      fulfill(response);
-      update(request);
-    }, reject);
-  });
-
-// fetch the resource from the browser cache
-const fromCache = request =>
-  caches
-    .open(CURRENT_CACHE)
-    .then(cache =>
-      cache
-        .match(request)
-        .then(matching => matching || cache.match('/offline/'))
-    );
-
-// cache the current page to make it available for offline
-const update = request =>
-  caches
-    .open(CURRENT_CACHE)
-    .then(cache =>
-      fetch(request).then(response => cache.put(request, response)
-    ).then(console.log("Cached this page"))
-    );
-
-// general strategy when making a request (eg if online try to fetch it
-// from the network with a timeout, if something fails serve from cache)
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    fromNetwork(evt.request, 1000).catch(() => fromCache(evt.request))
   );
-  evt.waitUntil(update(evt.request));
 });
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("[PWA Builder] add page to offline cache: " + response.url);
+
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+
+        return response;
+      })
+      .catch(function (error) {
+        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return the offline page
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        // The following validates that the request was for a navigation to a new document
+        if (request.destination !== "document" || request.mode !== "navigate") {
+          return Promise.reject("no-match");
+        }
+
+        return cache.match(offlineFallbackPage);
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
