a431ede4ebd76a3a88e6a74befb8e8ba79d8d401
Ahmed Thahir <ahmedthahir2002@gmail.com>
2023-03-26 14:17:49 +0400

    Update pwabuilder-sw.js

--- docs/pwabuilder-sw.js
+++ docs/pwabuilder-sw.js
@@ -3,7 +3,7 @@
 const CACHE = "pwabuilder-offline-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "/offline/index.html";
+const offlineFallbackPage = "/offline/";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
