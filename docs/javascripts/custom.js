4af265a900ae5919d167bfd7bdee2f13d5d27436
Ahmed Thahir <ahmedthahir2002@gmail.com>
2023-03-27 13:01:18 +0400

    Update custom.js

--- docs/javascripts/custom.js
+++ docs/javascripts/custom.js
@@ -1,21 +1,4 @@
 window.addEventListener("load", function(){
-  const regex_pattern = /([0-9]+[_ ])([A-Za-z0-9])/
-  const regex_replace = "$2"
-  
-  const non_footer_links = document.querySelectorAll("a:not(footer a)");
-  non_footer_links.forEach(function(link){
-      link.textContent = link.textContent.replace(regex, "")
-  });
-
-  const footer_directions = document.querySelectorAll(".md-footer__direction");
-  footer_directions.forEach(function(direction) {
-      direction.remove()
-  })
-
-  const footer_links = document.querySelectorAll("footer .md-ellipsis");
-  footer_links.forEach(function(link){
-      link.textContent = link.textContent.replace(regex, "")
-  });
   
   function prefetch(link, type)
   {
